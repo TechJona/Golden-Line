dedb7b2b9eb384f5673ad2379578cfac6690e395
Jonathan Jimenez <luisjjc99@gmail.com>
2025-01-29 23:32:16 -0500

    video 9 curso gas avanzado

--- funciones.js
+++ funciones.js
@@ -7,3 +7,9 @@ function doGet(){
 function obtenerDatosHTML(nombre){
    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos(){
+   let hoja = SpreadsheetApp.openById('1G0Jt0WyEQ4vc0ExseKJitmSOCuYvuMXRd5Smp_hk2ag').getActiveSheet();
+   let datos = hoja.getDataRange().getValues();
+   return datos;
+}
